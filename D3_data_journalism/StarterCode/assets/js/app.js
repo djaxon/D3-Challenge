011e5e8e813d0deabe67b02b4af0e399b8b17b4b
Dan Jackson <djacks11784@gmail.com>
2020-10-08 19:52:19 -0600

    Updated app.js file
    refreshed scatter plot w/ labels

--- D3_data_journalism/StarterCode/assets/js/app.js
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -12,8 +12,8 @@ d3.csv("assets/js/data.csv").then(function(journalismData) {
     })
 
 // set the dimensions and margins of the graph
-var margin = {top: 30, right: 30, bottom: 30, left: 30},
-    width = 460 - margin.left - margin.right,
+var margin = {top: 30, right: 30, bottom: 50, left: 50},
+    width = 800 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
@@ -23,34 +23,47 @@ var svg = d3.select("#scatter")
             .attr("height", height + margin.top + margin.bottom)
             .append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-// Add X Axis
+
+            // Add X Axis
 var x = d3.scaleLinear()
-            .domain([0, d3.max(journalismData, d=>d.poverty)])
+            .domain([d3.min(journalismData, d=>d.smokes)*.75, d3.max(journalismData, d=>d.smokes)])
             .range([ 0, width ]);
             svg.append("g")
             .attr("transform", "translate(0," + height + ")")
             .call(d3.axisBottom(x));
+            svg.append("text")
+            .text("Smokes (%)")
+            .attr("x", width/2 )
+            .attr("y", height + margin.bottom )
+            .classed("aText", true);
             
         
 // Add Y axis
 var y = d3.scaleLinear()
-            .domain([0, d3.max(journalismData, d=>d.healthcare)])
+            .domain([d3.min(journalismData, d=>d.age)*.75, d3.max(journalismData, d=>d.age)])
             .range([ height, 0]);
             svg.append("g")
             .call(d3.axisLeft(y));
-// Add dots
-            svg.append('g')
-            .selectAll("dots")
-            .data(journalismData)
-            .enter()
-            .append("circle")
-            // .statecircle()
-            .attr("cx", function (d) { return x(d.poverty); } )
-            .attr("cy", function (d) { return y(d.healthcare); } )
-            .attr("r", 8)
-            .classed("stateCircle", true) 
+            svg.append("text")
+            .text("Age (Yrs)")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 0-margin.left*.75 )
+            .attr("x", 0 - (height / 2))
+            .classed("aText",true);
             
-            .attr("text", function (d) {return (d.state);} ) 
-            .classed("stateText", true)          
-})
+// Add dots
+var gdots=  svg.selectAll("g.dot")
+            .data(journalismData)
+            .enter().append('g');
+            gdots.append("circle")
+            .attr("cx", function (d) { return x(d.smokes); } )
+            .attr("cy", function (d) { return y(d.age); } )
+            .attr("r", 10)
+            .classed("stateCircle", true); 
+            gdots.append("text")
+            .text(function (d) {return (d.abbr);} )
+            .attr("x", function (d) { return x(d.smokes); } )
+            .attr("y", function (d) { return y(d.age); } )
+            .classed("stateText", true);          
+});
 
